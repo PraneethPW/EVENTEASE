75e73f807b96f406b1d1f7b086676ba1fd01e7c1
Drake0112 <praneethreddy0112@gmail.com>
2026-01-01 16:43:42 +0530

    added /api

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL ||  "eventease-production-9ab0.up.railway.app",
+  baseURL: import.meta.env.VITE_BACKEND_URL ||  "eventease-production-9ab0.up.railway.app/api",
 });
 
 export default API;
